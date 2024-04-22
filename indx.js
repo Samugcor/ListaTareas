79b7da116458e60a3640b18b8120581023ff3e89
Samuel <samueligcorp@gmail.com>
2024-04-22 18:45:17 +0200

    algo

--- indx.js
+++ indx.js
@@ -1,4 +1,8 @@
 $(function(){
-
+$('#taskInput').change(function(){
+    let input = $(this).val();
+    $('ul').append('<li class="sincompletar"><div class="checkckontainer"><img src="Imagenes/square.svg" alt=""></div>'+input+'</li>');
+    $(this).val('')
+})
 
 })
