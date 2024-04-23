b2d99c615ec0e9cb3f983d932f0b83b5c94d2013
Samugcor <samueligcorp@gmail.com>
2024-04-23 13:04:55 +0200

    Terminado

--- indx.js
+++ indx.js
@@ -1,8 +1,32 @@
 $(function(){
-$('#taskInput').change(function(){
-    let input = $(this).val();
-    $('ul').append('<li class="sincompletar"><div class="checkckontainer"><img src="Imagenes/square.svg" alt=""></div>'+input+'</li>');
-    $(this).val('')
-})
+
+    $('#sendinput').on("click",function(){
+        let input = $('#taskInput').val();
+        $('ul').append('<li class="sincompletar"><div class="checkckontainer"><img src="Imagenes/square.svg" alt=""></div>'+input+'<div class="trashcontainer"><img src="Imagenes/trash-2.svg" alt=""></div></li>');
+        $('#taskInput').val('');
+    })
+
+    $('ul').on("mouseenter", 'li', function(){
+        $(this).children('.trashcontainer').css("display","flex");
+    })
+    $('ul').on("mouseleave",'li', function(){
+        $(this).children('.trashcontainer').css("display","none");
+    })
+
+    $('ul').on("click", '.checkckontainer',function(){
+        if($(this).children('img').attr('src')=="Imagenes/square.svg"){
+            $(this).children('img').attr('src','Imagenes/square-check-big.svg');
+        }
+        else{
+            $(this).children('img').attr('src','Imagenes/square.svg');
+        }
+        
+        $(this).parent('li').toggleClass("completada sincompletar");
+    });
+
+    $('ul').on("click", '.trashcontainer',function(){
+        
+        $(this).parent('li').fadeOut(100);
+    });
 
 })
